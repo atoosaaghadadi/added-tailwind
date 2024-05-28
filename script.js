b038e20a557de037429efd06f197640459e62a2e
atoosa <atoosaaghadadi78@gmail.com>
2024-05-28 00:43:35 -0700

    added p1 commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// var elm=document.getElementById('food');
+// function myfunc(){alert('You choose food :)');}
+// elm.addEventListener('click',function(){});
+
+
+function myfunc(){   
+    alert('you choose food item :)');
+}
+
+function myfunc2(){
+    alert('you choose plan item :)')
+}
